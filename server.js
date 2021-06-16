4b6f0dc58898fa28201b2b57d3f773378b125649
emerson-acacio-fs <emerson.acacio2@gmail.com>
2021-06-16 07:54:27 -0300

    parâmetros

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require("express");
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(
     '<form action="/"  method="POST"> Nome: <input type="text" name="nome"> <button>Enviar arquivo</button></form> '
   );
 });
-app.post("/", (req, res) => {
-  res.send("Recebio formulário!");
+app.get("/testes/:idUsuarios?", (req, res) => {
+  console.log(req.params);
+  console.log(req.query);
+  res.send("oi");
 });
-
-app.get("/contato", (req, res) => {
-  res.send("Obrigado por entrar em contato!");
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Recebio formulário!");
 });
 
 app.listen(3001, () => {
